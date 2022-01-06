79da23ec3c1986a43f795111daf641bfceb589d5
7248510 <calebkre19@gmail.com>
2022-01-05 23:56:32 -0500

    Cleaning main branch

--- FastAI-PyTorch/fastai.bat
+++
@@ -1,4 +0,0 @@
-echo "This assumes conda is in your path(Reinstalling Conda is not a problem as long as your know how paths/sys vars work)"
-conda install pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch -c conda-forge
-conda install -c nvidia -c fastai fastai anaconda
-echo "These commands will set up pytorch and Fast AI on a local computer with Cuda."
